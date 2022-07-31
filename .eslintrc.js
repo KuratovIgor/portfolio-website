e6ed3d42aa289549b895460496b23222887d9e29
Igor Kuratov <kuratov.ia@dclouds.ru>
2022-07-31 18:40:52 +0700

    project configuration, git init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'import/no-unresolved': 'off',
+    'no-plusplus': 'off',
+    'import/extensions': [
+      'error',
+      'never',
+    ],
+    'no-restricted-syntax': ['error', 'BinaryExpression[operator=\'in\']'],
+  },
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home-page">
+    <h1 class="home-page__title">Hello world</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-page {
+  &__title {
+    margin: 0;
+    font-size: 20px;
+  }
+}
+</style>
